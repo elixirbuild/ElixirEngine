dbbdd3fc20a84d5ca8307f064a75b8b54aeeea87
Elixir <63171099+elixirbuild@users.noreply.github.com>
2020-08-31 19:56:09 -0500

    Create package.js

---
+++ package/package.js
@@ -0,0 +1,13 @@
+// This is a module we used for packaging a game
+
+var json = {
+    name: "my-game",
+    description: "My EET1 game.",
+    product_name: "my-game",
+    liscense: "MIT"
+};
+
+var n = json.name;
+var d = json.description;
+var pn = json.product_name;
+var l = json.liscense;
