cc05f2bbe91e54d1f08b7bcf80a685f06345cb2b
Elixir <63171099+elixirbuild@users.noreply.github.com>
2020-08-31 20:04:49 -0500

    Update package.js

--- package/package.js
+++ package/package.js
@@ -1,6 +1,6 @@
 // This is a module we used for packaging a game
 
-// This is the default information for the game that we usually change when making a game
+// This is the default information for the game that we usually change after making a game
 var json = {
     name: "my-game",
     description: "My EET1 game.",
