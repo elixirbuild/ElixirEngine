ef62257e13439d334b8ee3d106fb2c2df1787aee
Elixir <63171099+elixirbuild@users.noreply.github.com>
2020-09-29 18:36:42 -0500

    Update elixir-engine-one.js

--- src/elixir-engine-one.js
+++ src/elixir-engine-one.js
@@ -148,3 +148,45 @@ function Rotate(width, height, color, x, y) {
         ctx.restore();
     }
 }
+
+// 1.1.2
+var GameArea = {
+    canvas: document.getElementById("world"),
+    start: function () {
+        this.canvas.width = 1000;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+
+        window.addEventListener('keydown', function (e) {
+            GameArea.keys = (GameArea.keys || []);
+            GameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            GameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+    },
+    stop: function () {
+        clearInterval(this.interval);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+// audio
+function Audio(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+    this.stop = function () {
+        this.sound.pause();
+    }
+}
